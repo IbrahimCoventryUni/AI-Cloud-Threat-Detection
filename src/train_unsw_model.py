b5d5a6ee4b26118b5cb79b3ba89c1e4065f07740
Ibrahim <ibrahim14.m10@gmail.com>
2025-02-24 01:03:40 +0000

    UNSW data training

--- src/train_unsw_model.py
+++ src/train_unsw_model.py
@@ -18,6 +18,22 @@ y = df["label"]
 # Split dataset (80% train, 20% test)  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  
 
+
+# Fill missing values in sport & dsport with most frequent value (mode)
+X_train["sport"].fillna(X_train["sport"].mode()[0], inplace=True)
+X_train["dsport"].fillna(X_train["dsport"].mode()[0], inplace=True)
+X_test["sport"].fillna(X_test["sport"].mode()[0], inplace=True)
+X_test["dsport"].fillna(X_test["dsport"].mode()[0], inplace=True)
+
+# Drop ct_ftp_cmd because it has too many missing values
+X_train.drop(columns=["ct_ftp_cmd"], inplace=True, errors="ignore")
+X_test.drop(columns=["ct_ftp_cmd"], inplace=True, errors="ignore")
+
+# Fill missing values in ct_ftp_cmd with 0
+X_train["ct_ftp_cmd"].fillna(0, inplace=True)
+X_test["ct_ftp_cmd"].fillna(0, inplace=True)
+
+
 # Define models  
 models = {  
     "Random Forest": RandomForestClassifier(n_estimators=100, random_state=42),  
