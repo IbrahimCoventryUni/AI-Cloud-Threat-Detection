055f2b8febb38a1e3e59d34429147cc7d340398d
Ibrahim141010 <107619310+Ibrahim141010@users.noreply.github.com>
2025-04-14 07:01:03 +0100

    Update config.py
    removing creds

--- src/config.py
+++ src/config.py
@@ -3,18 +3,18 @@
 EMAIL_CONFIG = {
     "EMAIL_SUBJECT": "Alert: Possible Cyber Attack Detected",
     "EMAIL_BODY": "A potential attack has been detected by the threat detection system. Please investigate immediately.",
-    "TO_EMAIL": ["ibrahimaithreat@gmail.com", "mahaboobbi@coventry.ac.uk"],
-    "FROM_EMAIL": "ibrahimaithreat@gmail.com",
+    "TO_EMAIL": ["my_email_xxx@xxx.com", "my_email_xxx@xxx.com"],
+    "FROM_EMAIL": "my_email_xxx@xxx.com",
     "SMTP_SERVER": "smtp.gmail.com",
     "SMTP_PORT": 587,
-    "USERNAME": "ibrahimaithreat@gmail.com",
-    "PASSWORD": "nstq bcif yier lvfr"  # Your app password
+    "USERNAME": "my_email_xxx@xxx.com",
+    "PASSWORD": "my_actual_password123"
 }
 
 # Telegram configuration settings
 TELEGRAM_CONFIG = {
-    "BOT_TOKEN": "7849697100:AAEj_QP5i23TR7NJgfHZQ8O0EMxwth2vTVY",
-    "CHAT_ID": "6438891757"
+    "BOT_TOKEN": "my_actual_token123",
+    "CHAT_ID": "my_actual_token"
 }
 
 # Global alert cooldown period (in seconds)
